bcd37a5cf2fbcb0381da0a79ca11759793701049
aaron <aaronjsunk@gmail.com>
2022-09-15 10:23:09 +0100

    Try run NPM from setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import json
-from setuptools import setup
+
+import setuptools
+from setuptools.command.egg_info import egg_info
+from setuptools.command.install import install
+from setuptools.command.develop import develop
 
 with open("package.json") as f:
     package = json.load(f)
@@ -9,7 +13,52 @@ with open("docs/README-PyPi.md", encoding="utf-8") as f:
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
 
-setup(
+
+class NPMInstallEgg(egg_info):
+    def run(self):
+        self.run_command("npm install")
+        egg_info.run(self)
+
+
+class NPMInstall(install):
+    def run(self):
+        self.run_command("npm install")
+        install.run(self)
+
+
+class NPMInstallDevelop(develop):
+    def run(self):
+        self.run_command("npm install")
+        develop.run(self)
+
+
+class NPMBuildEgg(egg_info):
+    def run(self):
+        self.run_command("npm run build")
+        egg_info.run(self)
+
+
+class NPMBuildInstall(install):
+    def run(self):
+        self.run_command("npm run build")
+        install.run(self)
+
+
+class NPMBuildDevelop(develop):
+    def run(self):
+        self.run_command("npm run build")
+        develop.run(self)
+
+
+setuptools.setup(
+    cmdclass={
+        'npm_installI': NPMInstall,
+        'npm_installE': NPMInstallEgg,
+        'npm_installD': NPMInstallDevelop,
+        'npm_buildD': NPMBuildDevelop,
+        'npm_buildE': NPMBuildEgg,
+        'npm_buildI': NPMBuildInstall,
+    },
     name=package_name,
     version=package["version"],
     author=package["author"],
